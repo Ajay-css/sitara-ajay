c75a3b8e700d69dce7b88ccaa2ad9cf7baf80442
ajayarumugam73@gmail.com <you@example.com>
2025-07-27 12:02:42 +0530

    whatsapp

--- app.js
+++ app.js
@@ -146,7 +146,7 @@ function takeCommand(message) {
     }
     else if (message.includes("open whatsapp") || message.includes("open the whatsapp")) {
         speak("Sure sir , Opening WhatsApp");
-        window.open("whatsapp://");
+        window.open("Whatsapp://");
     }
     else if (message.includes("open gmail") || message.includes("open the gmail")) {
         speak("Sure sir , Opening Gmail");

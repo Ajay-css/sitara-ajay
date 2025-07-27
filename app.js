d07be739c743b73815a0bc3cf5e529a39470e106
ajayarumugam73@gmail.com <you@example.com>
2025-07-27 11:59:21 +0530

    123456789009

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ function takeCommand(message) {
     else if (message.includes("i love you")) {
         speak("I Love You Paaandi Maama");
     }
-    else if (message.includes("meena paytm")) {
+    else if (message.includes("i hate you")) {
         speak("I Love You Meena Akka");
     }
     else if (message.includes("open visual studio code") || message.includes("open vs code")) {
